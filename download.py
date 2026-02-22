13b276800c234de13948dbcca0229a79b7cc697a
Cobbleass <asscobble@gmail.com>
2026-02-23 02:20:09 +0530

    Update download.py

--- download.py
+++ download.py
@@ -8,7 +8,7 @@ SKIA_RELEASE_FILE_NAME = "Skia-Windows-Release-x64.zip"
 
 def get_latest_tag_aseprite():
     response = requests.get(
-        f"hhttps://github.com/aseprite/aseprite.git"
+        f"https://github.com/aseprite/aseprite.git"
     )
     response_json = response.json()
 
